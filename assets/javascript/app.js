4d25c98830788c26cda28a7807d5fc946cfef5b3
brianalika <brian.alika.clark@gmail.com>
2018-06-24 20:39:05 -0600

    Revised Files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
-var panel = $("#quiz-area");
-var countStartNumber = 30;
+var deck = $("#quiz-section");
+var countDown = 15;
 
 var questions = [{
   question: "In which city was the United Nations formed?",
@@ -44,48 +44,46 @@ var questions = [{
   image: "assets/images/switzerland.png"
 }];
 
-// Variable to hold our setInterval
 var timer;
 var game = {
 
   questions: questions,
-  currentQuestion: 0,
-  counter: countStartNumber,
+  historyQuestion: 0,
+  counter: countDown,
   correct: 0,
   incorrect: 0,
 
   countdown: function() {
     game.counter--;
-    $("#counter-number").html(game.counter);
+    $("#number").html(game.counter);
     if (game.counter === 0) {
-      console.log("TIME UP");
       game.timeUp();
     }
   },
 
   loadQuestion: function() {
     timer = setInterval(game.countdown, 1000);
-    panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
-    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-      panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
-      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+    deck.html("<h2>" + questions[this.historyQuestion].question + "</h2>");
+    for (var i = 0; i < questions[this.historyQuestion].answers.length; i++) {
+      deck.append("<button class='answer-button' id='button' data-name='" + questions[this.historyQuestion].answers[i]
+      + "'>" + questions[this.historyQuestion].answers[i] + "</button>");
     }
   },
 
   nextQuestion: function() {
-    game.counter = countStartNumber;
-    $("#counter-number").html(game.counter);
-    game.currentQuestion++;
+    game.counter = countDown;
+    $("#number").html(game.counter);
+    game.historyQuestion++;
     game.loadQuestion();
   },
 
   timeUp: function() {
     clearInterval(timer);
     $("#counter-number").html(game.counter);
-    panel.html("<h2>Out of Time!</h2>");
-    panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
-    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
-    if (game.currentQuestion === questions.length - 1) {
+    deck.html("<h2>Times Up!</h2>");
+    deck.append("<h3>The correct response was: " + questions[this.historyQuestion].correctAnswer);
+    deck.append("<img src='" + questions[this.historyQuestion].image + "' />");
+    if (game.historyQuestion === questions.length - 1) {
       setTimeout(game.results, 6 * 1000);
     }
     else {
@@ -95,17 +93,17 @@ var game = {
 
   results: function() {
     clearInterval(timer);
-    panel.html("<h2>Pop quiz over! Here is how you did...</h2>");
-    $("#counter-number").html(game.counter);
-    panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
-    panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
-    panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
-    panel.append("<br><button id='start-over'>Start Over?</button>");
+    deck.html("<h2>Pop quiz over! Here is how you did...</h2>");
+    $("#number").html(game.counter);
+    deck.append("<h3>Correct Answers: " + game.correct + "</h3>");
+    deck.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+    deck.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
+    deck.append("<br><button id='start-over'>Start Over?</button>");
   },
 
   clicked: function(e) {
     clearInterval(timer);
-    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+    if ($(e.target).attr("data-name") === questions[this.historyQuestion].correctAnswer) {
       this.answeredCorrectly();
     }
     else {
@@ -116,10 +114,10 @@ var game = {
   answeredIncorrectly: function() {
     game.incorrect++;
     clearInterval(timer);
-    panel.html("<h2>Nope!</h2>");
-    panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
-    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
-    if (game.currentQuestion === questions.length - 1) {
+    deck.html("<h2>Nope!</h2>");
+    deck.append("<h3>The correct answer was: " + questions[game.historyQuestion].correctAnswer + "</h3>");
+    deck.append("<img src='" + questions[game.historyQuestion].image + "' />");
+    if (game.historyQuestion === questions.length - 1) {
       setTimeout(game.results, 6 * 1000);
     }
     else {
@@ -130,9 +128,9 @@ var game = {
   answeredCorrectly: function() {
     clearInterval(timer);
     game.correct++;
-    panel.html("<h2>Correct!</h2>");
-    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
-    if (game.currentQuestion === questions.length - 1) {
+    deck.html("<h2>Correct!</h2>");
+    deck.append("<img src='" + questions[game.historyQuestion].image + "' />");
+    if (game.historyQuestion === questions.length - 1) {
       setTimeout(game.results, 6 * 1000);
     }
     else {
@@ -141,15 +139,13 @@ var game = {
   },
 
   reset: function() {
-    this.currentQuestion = 0;
-    this.counter = countStartNumber;
+    this.historyQuestion = 0;
+    this.counter = countDown;
     this.correct = 0;
     this.incorrect = 0;
     this.loadQuestion();
   }
 };
-
-// CLICK EVENTS
 
 $(document).on("click", "#start-over", function() {
   game.reset();
@@ -160,6 +156,6 @@ $(document).on("click", ".answer-button", function(e) {
 });
 
 $(document).on("click", "#start", function() {
-  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  $("#sub-box").prepend("<h2>Time Remaining: <span id='number'>15</span> Seconds</h2>");
   game.loadQuestion();
 });
